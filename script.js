c99fd4c3d8355c125287171d57f1bc04f1c095ea
Natanev92 <nvillasenor1@icloud.com>
2021-05-25 20:23:31 -0700

    dojo weather

---
+++ script.js
@@ -0,0 +1,10 @@
+function btn(){
+    alert("Loading weather repot...")
+}
+function bottom(){
+    var myElem = document.getElementById('message')
+    console.log(myElem);
+    myElem.style.display = "none";
+}
+// function button2(){
+    // alert("try again!")}
